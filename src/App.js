88c7bbc753d6a5c39bdae66cb8b719b7c9460f4b
ninesky00 <jiangzho@gmail.com>
2021-06-20 18:56:03 -0500

    add expenses and ui folder to better organize components, add change title button to change state of title in ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Expenses from './components/Expenses'
+import Expenses from "./components/expenses/Expenses";
 
+// const App = () => {
 function App() {
   const expenses = [
     {
@@ -27,6 +28,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
