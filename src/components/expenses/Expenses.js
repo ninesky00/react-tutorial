e4e27ae04ed9a419f0bc283cb8ead6a030f9d9fa
ninesky00 <jiangzho@gmail.com>
2021-06-23 21:37:46 -0500

    update App.js to output new expense that we passed to be added to the expenses array using useState, fix ExpenseItem.js to output props.title instead of just title to reflect changes

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
-import Card from '../ui/Card';
-import './Expenses.css';
-import ExpensesFilter from './ExpensesFilter';
+import Card from "../ui/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (date) => {
-    setFilteredYear(date)
-  }
+    setFilteredYear(date);
+  };
   return (
     <div>
-      <Card className="expenses">      
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
-  )
+  );
 }
 
 export default Expenses;
