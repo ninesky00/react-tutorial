b656f9b322913f13f0b81b279b11f4cc0c03c2e8
ninesky00 <jiangzho@gmail.com>
2021-06-29 17:07:37 -0500

    extract ExpensesList component out of ExpenseItem to output conditional content

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../ui/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from './Expenseslist';
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,9 +11,11 @@ function Expenses(props) {
     setFilteredYear(date);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+
 
   return (
     <div>
@@ -22,14 +24,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList expenses={filteredExpenses}/>
       </Card>
     </div>
   );
