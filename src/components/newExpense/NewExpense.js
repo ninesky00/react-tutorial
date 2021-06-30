039065e8591fd01a168292ccaf1c98987b30fcf4
ninesky00 <jiangzho@gmail.com>
2021-06-30 18:00:26 -0500

    add conditional output for add new expense button such that only a button is displayed until clicked, then the add expense form is displayed.

--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,17 +1,31 @@
+import React, { useState } from 'react';
+
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = (props) => {
+  const [editing, setEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setEditing(false);
   };
 
-  return <div className="new-expense">
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+  const startEditingHandler = () => {
+    setEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setEditing(false);
+  }
+
+  return <div className="new-expense button">
+    {!editing && <button onClick={startEditingHandler}>Add New Expense</button>}
+    {editing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
   </div>
 };
 
